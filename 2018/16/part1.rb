0614eb23cc12265776a89d86cc6fe7e68f7e9c48
Josh Lee <josh.lee@parachutehealth.com>
2018-12-28 18:04:19 -0500

    Refactor

--- 2018/16/part1.rb
+++ 2018/16/part1.rb
@@ -16,34 +16,52 @@ def parse_instr(str)
   str.split.map(&:to_i)
 end
 
-ops = {
-  addr: -> (r,a,b,c) { r[c] = r[a] + r[b] },
-  addi: -> (r,a,b,c) { r[c] = r[a] + b },
-  mulr: -> (r,a,b,c) { r[c] = r[a] * r[b] },
-  muli: -> (r,a,b,c) { r[c] = r[a] * b },
-  banr: -> (r,a,b,c) { r[c] = r[a] & r[b] },
-  bani: -> (r,a,b,c) { r[c] = r[a] & b },
-  borr: -> (r,a,b,c) { r[c] = r[a] | r[b] },
-  bori: -> (r,a,b,c) { r[c] = r[a] | b },
-  setr: -> (r,a,b,c) { r[c] = r[a] },
-  seti: -> (r,a,b,c) { r[c] = a },
-  gtir: -> (r,a,b,c) { r[c] = a > r[b] ? 1 : 0 },
-  gtri: -> (r,a,b,c) { r[c] = r[a] > b ? 1 : 0 },
-  gtrr: -> (r,a,b,c) { r[c] = r[a] > r[b] ? 1 : 0 },
-  eqir: -> (r,a,b,c) { r[c] = a == r[b] ? 1 : 0 },
-  eqri: -> (r,a,b,c) { r[c] = r[a] == b ? 1 : 0 },
-  eqrr: -> (r,a,b,c) { r[c] = r[a] == r[b] ? 1 : 0 }
-}.freeze
-
-num_samples = 0
-each_sample(ARGV[0]) do |reg_pre, instr, reg_post|
-  acts_like_count = ops.count do |opcode, proc|
-    regs = reg_pre.dup
-    proc.call(regs, *instr.slice(1,3))
-    regs == reg_post
-  end
-
-  num_samples += 1 if acts_like_count >= 3
+def op_procs
+  @op_procs ||= {
+    addr: -> (r,a,b,c) { r[c] = r[a] + r[b] },
+    addi: -> (r,a,b,c) { r[c] = r[a] + b },
+    mulr: -> (r,a,b,c) { r[c] = r[a] * r[b] },
+    muli: -> (r,a,b,c) { r[c] = r[a] * b },
+    banr: -> (r,a,b,c) { r[c] = r[a] & r[b] },
+    bani: -> (r,a,b,c) { r[c] = r[a] & b },
+    borr: -> (r,a,b,c) { r[c] = r[a] | r[b] },
+    bori: -> (r,a,b,c) { r[c] = r[a] | b },
+    setr: -> (r,a,b,c) { r[c] = r[a] },
+    seti: -> (r,a,b,c) { r[c] = a },
+    gtir: -> (r,a,b,c) { r[c] = a > r[b] ? 1 : 0 },
+    gtri: -> (r,a,b,c) { r[c] = r[a] > b ? 1 : 0 },
+    gtrr: -> (r,a,b,c) { r[c] = r[a] > r[b] ? 1 : 0 },
+    eqir: -> (r,a,b,c) { r[c] = a == r[b] ? 1 : 0 },
+    eqri: -> (r,a,b,c) { r[c] = r[a] == b ? 1 : 0 },
+    eqrr: -> (r,a,b,c) { r[c] = r[a] == r[b] ? 1 : 0 }
+  }.freeze
 end
 
-p num_samples
+def opcodes
+  op_procs.keys
+end
+
+def do_op(opcode, registers, a, b, c=nil)
+  @op_procs[opcode].call(registers, a, b, c)
+end
+
+def sample_acts_like_op?(opcode, reg_pre, instr, reg_expected)
+  regs = reg_pre.dup
+  do_op(opcode, regs, *instr.slice(1,3))
+  regs == reg_expected
+end
+
+def part1
+  num_samples = 0
+  each_sample(ARGV[0]) do |reg_pre, instr, reg_post|
+    potential_ops = opcodes.count do |opcode|
+      sample_acts_like_op?(opcode, reg_pre, instr, reg_post)
+    end
+    num_samples += 1 if potential_ops >= 3
+  end
+
+  puts "#{num_samples} samples act like 3 or more ops"
+end
+
+part1
+
