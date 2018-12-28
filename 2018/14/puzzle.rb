e67c66778e01733afc15c61216548486f255008d
Josh Lee <josh.lee@parachutehealth.com>
2018-12-27 19:38:24 -0500

    Finish 14.2

--- 2018/14/puzzle.rb
+++ 2018/14/puzzle.rb
@@ -19,5 +19,38 @@ def part1(next_ten_after)
   puts "After #{next_ten_after} recipes, the scores of the next ten are #{scores}."
 end
 
-input = ARGV[0].to_i
-part1(input)
+def part2(seek_pattern)
+  recipes = [3, 7]
+  elf1 = 0
+  elf2 = 1
+
+  tail = recipes.dup
+  seek = seek_pattern.chars.map(&:to_i)
+
+  loop do
+    sum = recipes[elf1] + recipes[elf2]
+
+    keep_going = sum.to_s.chars.each do |new_recipe|
+      recipe = new_recipe.to_i
+
+      recipes.push(recipe)
+      tail.push(recipe)
+      tail.shift if tail.length > seek.length
+
+      break if tail == seek
+    end
+
+    break unless keep_going
+
+    elf1 = (elf1 + 1 + recipes[elf1]) % recipes.length
+    elf2 = (elf2 + 1 + recipes[elf2]) % recipes.length
+
+    print "#{recipes.length.to_s.ljust(10)}\r"
+  end
+
+  puts "#{seek_pattern} first appears after #{recipes.length - seek.length} recipes"
+end
+
+input = ARGV[0]
+part1(input.to_i)
+part2(input)
